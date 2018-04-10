cb1f98a3415d7debb2ca44d67cf1d5c52ed133b7
arenaguiose <37896208+arenaguiose@users.noreply.github.com>
2018-04-10 17:54:30 +0200

    new structure with delays

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,18 +1,20 @@
 // Initialize
 
 var data = null;
-var dialogue = null;
-var answer = null;
-var delay = 500;
+var currentDialogue = null;
+var currentMessage = null;
+var answerChosen = null;
+var delay = 0;
+var dialogueIndex = 0;
 
 // Start
 
 $(function() {
   $.getJSON( 'scenarii/noemie.json', function( dataLoaded ) {
     data = dataLoaded;
-    dialogue = data.dialogue
+    currentDialogue = data.dialogue
     setTitle();
-    otherTalks();
+    showCurrentMessage();
   });
 });
 
@@ -22,53 +24,79 @@ function setTitle() {
   $('.title').html(data.characters.other.name);
 }
 
-function otherTalks() {
+function showCurrentMessage() {
+  console.log("showCurrentMessage");
+  currentMessage = currentDialogue[dialogueIndex];
+  delay = currentMessage.delay;
   setTimeout(function() {
-    if (dialogue.other === undefined) {
-      end();
+    if (currentMessage.other) {
+      otherTalks();
     } else {
-      $('.chatlogs').append('<div class="chat friend"><div class="user-photo"><img src="' + data.characters.other.photo + '" class="one"></div><p class="chat-message">' + dialogue.other + '</p></div>');
       iTalk();
     }
+    autoScroll();
   }, delay);
+}
+
+function autoScroll() {
+  $('.chatlogs').scrollTop($('.chatlogs')[0].scrollHeight);
+}
+
+function planNextMessage() {
+  console.log("planNextMessage");
+  dialogueIndex++;
+  if (currentDialogue[dialogueIndex] === undefined) {
+    setTimeout(end, 3000);
+  } else {
+    showCurrentMessage();
+  }
+}
+
+function otherTalks(){
+  console.log("otherTalks");
+  $('.chatlogs').append('<div class="chat friend"><div class="user-photo"><img src="' + data.characters.other.photo + '" class="one"></div><p class="chat-message">' + currentMessage.other + '</p></div>');  
+  planNextMessage();
 }
 
 function iTalk() {
-  setTimeout(function() {
-    if (dialogue.me.length == 1) {
-      iAutoAnswer();
-    } else {
-      iChoose();
-    }  
-  }, delay);
+  console.log("iTalk");
+  if (typeof(currentMessage.me) === 'string') {
+    iAutoAnswer();
+  } else {
+    iChoose();
+  }
 }
 
 function iAutoAnswer() {
-  answer = dialogue.me[0];
-  iAnswer();
-}
-
-function iAnswer() {
-  $('.chatlogs').append('<div class="chat self"><img src="' + data.characters.me.photo + '" class="two"><div class="user-photo"></div><p class="chat-message">' + dialogue.me[0].name + '</p></div>');
-  dialogue = answer.dialogue;
-  otherTalks();
+  console.log("iAutoAnswer");
+  $('.chatlogs').append('<div class="chat self"><img src="' + data.characters.me.photo + '" class="two"><div class="user-photo"></div><p class="chat-message">' + currentMessage.me + '</p></div>');
+  planNextMessage();
 }
 
 function iChoose() {
-  for (var i = 0; i < dialogue.me.length; i++) {
-    var option = dialogue.me[i];
-    $('.chatform').append('<div class="option" data-index="' + i + '">' + option.name + '</div>');
-    $('.chatform div').click(function() {
-      var index = $(this).data('index');
-      answer = dialogue.me[index];
+  console.log("iChoose");
+  for (var i = 0; i < currentMessage.me.length; i++) {
+    var option = currentMessage.me[i];
+    $('.chatform').append('<div class="option" data-answer-index="' + i + '">' + option.text + '</div>');
+    $('.chatform .option').unbind('click').bind('click', function() {
+      var answerIndex = $(this).data('answer-index');
       $('.chatform').html('');
+      answerChosen = currentMessage.me[answerIndex];
+      currentDialogue = answerChosen.dialogue;
+      dialogueIndex = -1;
       iAnswer();
     })
   }
 }
 
-
+function iAnswer() {
+  console.log("iAnswer");
+  $('.chatlogs').append('<div class="chat self"><img src="' + data.characters.me.photo + '" class="two"><div class="user-photo"></div><p class="chat-message">' + answerChosen.text + '</p></div>');
+  autoScroll();
+  planNextMessage();
+}
 
 function end() {
-  alert('FIN');
+  // console.log("end");
+  $('.messageFin').show();
 }
