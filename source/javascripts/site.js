5a7987bc0fa5f0e9286291b62c814ecc79f1ff8f
arenaguiose <37896208+arenaguiose@users.noreply.github.com>
2018-04-12 10:40:07 +0200

    debug message affichage complete

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -67,7 +67,7 @@ function otherTalks(){
   console.log("otherTalks");
   dialogueEllipsis = currentMessage.ellipsis;
   if (dialogueEllipsis > 0) {
-    $('.chatlogs').append('<div class ="ellipsis"><div class="user-photo"><img src="' + data.characters.other.photo + '" class="one"></div><p class="chat-message"><p class="saving"><span>.</span><span>.</span><span>.</span></p></div></div></div>');
+    $('.chatlogs').append('<div class ="ellipsis"><div class="chat friend"><div class="user-photo"><img src="' + data.characters.other.photo + '" class="one"></div><div class="chat-message"><p class="saving"><span>.</span><span>.</span><span>.</span></p></div></div></div>');
     setTimeout(function() { 
       $('.ellipsis').hide();
       $('.chatlogs').append('<div class="chat friend"><div class="user-photo"><img src="' + data.characters.other.photo + '" class="one"></div><p class="chat-message">' + currentMessage.other + '</p></div>');  
@@ -75,7 +75,7 @@ function otherTalks(){
     }, dialogueEllipsis);
   }
    else { 
-    $('.chatlogs').append('<div class="chat friend"><div class="user-photo"><img src="' + data.characters.other.photo + '" class="one"></div><p class="chat-message>' + currentMessage.other + '</p></div>');  
+    $('.chatlogs').append('<div class="chat friend"><div class="user-photo"><img src="' + data.characters.other.photo + '" class="one"></div><p class="chat-message">' + currentMessage.other + '</p></div>');  
   planNextMessage();
 }
 }
